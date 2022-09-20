7c48370c489d0ba3a8fd9b174e779d78ad3ec8b7
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2022-09-20 15:25:47 -0300

    Update Class UserRepository

--- src/main/java/com/springcourse/repository/UserRepository.java
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -13,7 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	public Optional<User> getByName(String nome); // Busca um usuáriio pelo nome
 	public Optional<User> getByEmail(String email); // Busca um usuario pelo e-mail
-	@Query("SELECT u FROM User u WHERE email = ?1 AND password = ?2") 
+	@Query("SELECT u FROM user u WHERE email= ?1 AND password= ?2") 
 	public Optional<User> login(String email, String password); // Realiza o login pelo email e pelo password
 	public Optional<User> findById(Long id); // Busca um usuário pelo ID
 	public Optional<User> findByAll();  // Busca todos os usuário
