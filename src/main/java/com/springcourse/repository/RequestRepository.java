ff10bae24d00100b7a6c04fc60a40e582e19234b
Marcilio Zanatta <103801733+marciliodevjava@users.noreply.github.com>
2022-09-22 13:47:22 -0300

    Update query RequestRepository

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -2,7 +2,10 @@ package com.springcourse.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +16,9 @@ import com.springcourse.domain.enums.RequestStageEnum;
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
 	public List<Request> findAllByOwnerId(Long id); // Retorna a lista de pedidos de um usuário
+	
+	@Transactional
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
-	public Request updateStatus(Long id, RequestStageEnum state);
+	public int updateStatus(Long id, RequestStageEnum state);
 }
